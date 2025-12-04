f2d831db035560cf976720f98855d97fafc54579
Tony Olivo <tony.olivo@gmail.com>
2025-12-04 12:52:22 +0000

    Day 3 pt 1 done, pt 2 in progress

--- day3/day3.go
+++ day3/day3.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
+	"strings"
 
 	"github.com/Thav/aoc2025/lists"
 )
@@ -21,11 +21,12 @@ func main() {
 	}
 	fmt.Println(string(example[0:10]))
 	fmt.Println(string(puzzle[0:10]))
-	input := puzzle
+	input := example
 	banks := lists.ImportRowListsInt(input, "")
 	joltage1 := 0
-	for _, bank := range banks {
-		bank = slices.Compact(bank)
+	joltage2 := 0
+	var b strings.Builder
+	for i, bank := range banks {
 		indices := make(map[int][]int, 0)
 		for i, jattery := range bank {
 			if jattIndices, ok := indices[jattery]; ok {
@@ -34,12 +35,15 @@ func main() {
 				indices[jattery] = []int{i}
 			}
 		}
-
 		j1 := part1logic(bank, indices)
+		j2 := part2logic(bank, indices)
+		b.WriteString(fmt.Sprintf("%d: %v \n %d\n", i, bank, j2))
 		joltage1 += j1
+		joltage2 += j2
 	}
 	fmt.Println("Part 1: ", joltage1)
-	fmt.Println("Part 2: ", "")
+	fmt.Println("Part 2: ", joltage2)
+	os.WriteFile("output.txt", []byte(b.String()), 0666)
 
 }
 
@@ -48,9 +52,6 @@ func part1logic(bank []int, indices map[int][]int) (joltage int) {
 		jattIndices, ok := indices[j]
 		if !ok || jattIndices[0] == (len(bank)-1) {
 			continue
-		}
-		if len(jattIndices) > 1 {
-			return j*10 + j
 		}
 		for k := 9; k > 0; k-- {
 			kjattIndices, ok := indices[k]
@@ -66,7 +67,39 @@ func part1logic(bank []int, indices map[int][]int) (joltage int) {
 	return 0
 }
 
-func part2logic(from, to string) (sum int) {
-
+func part2logic(bank []int, indices map[int][]int) (sum int) {
+	i := 0
+	lastIndex := 0
+	// Find the highest first digit with room for remaining digits behind it
+	// Remove that index from list
+	// Add that digit to the sum
+	// Increment digit counter
+	// track position of last digit
+	// Find next best digit following the first, remove it
+	// Find next best digit following the second ...
+DigitLoop:
+	for i < 12 {
+		for j := 9; j > 0; j-- {
+			jattIndices, ok := indices[j]
+			if !ok || len(jattIndices) == 0 {
+				continue
+				// Don't have any 9, 8, 7...
+			}
+			for len(jattIndices) > 0 && jattIndices[0] < lastIndex {
+				jattIndices = jattIndices[1:]
+				// Remove leading index when past the last digit added
+			}
+			if len(jattIndices) > 0 && jattIndices[0] > (len(bank)-1-(12-i)) {
+				continue
+				// Remaining locations of this digit can't be next, but might work later
+			}
+			// now we have a good digit
+			sum = sum*10 + j
+			i++
+			indices[j] = jattIndices[1:]
+			jattIndices, ok = indices[j]
+			continue DigitLoop
+		}
+	}
 	return
 }
